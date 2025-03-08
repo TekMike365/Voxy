762c875637adaac6eccf6f834c4fbc6c3225ad0e
TekMike365 <miskosklenka@gmail.com>
2025-03-08 16:45:32 +0100

    Created Application

---
+++ Voxy/Application.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Voxy
+{
+    class Application
+    {
+    public:
+        Application();
+        ~Application();
+
+        void Run();
+    };
+}
--- Voxy/main.cpp
+++ Voxy/main.cpp
@@ -1,6 +1,11 @@
 #include "Log.h"
 
+#include "Application.h"
+
 int main()
 {
     Voxy::Log::Init();
+
+    Voxy::Application app;
+    app.Run();
 }
