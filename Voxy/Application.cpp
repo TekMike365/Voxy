01d600067b9cd3e50524ebe1cce3c52d3fa0142e
TekMike365 <miskosklenka@gmail.com>
2025-03-24 18:16:03 +0100

    :3

--- Voxy/Application.cpp
+++ Voxy/Application.cpp
@@ -34,26 +34,28 @@ namespace Voxy
         {
             m_Window->OnUpdate();
 
-            // TODO: Cleanup
-            // Debug Screen
-            ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav;
-            const float PAD = 10.0f;
-            const ImGuiViewport *viewport = ImGui::GetMainViewport();
-            ImVec2 work_pos = viewport->WorkPos; // Use work area to avoid menu-bar/task-bar, if any!
-            ImVec2 work_size = viewport->WorkSize;
-            ImVec2 window_pos, window_pos_pivot;
-            window_pos.x = work_pos.x + work_size.x - PAD;
-            window_pos.y = work_pos.y + PAD;
-            window_pos_pivot.x = 1.0f;
-            window_pos_pivot.y = 0.0f;
-            ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
-            window_flags |= ImGuiWindowFlags_NoMove;
-            ImGui::SetNextWindowBgAlpha(0.35f); // Transparent background
-            if (ImGui::Begin("Example: Simple overlay", NULL, window_flags))
             {
-                ImGui::Text("%.3f ms (%.1f FPS)", dt.GetMiliseconds(), 1.0f / dt.GetSeconds());
+                // TODO: Cleanup
+                // Debug Screen
+                ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav;
+                const float PAD = 10.0f;
+                const ImGuiViewport *viewport = ImGui::GetMainViewport();
+                ImVec2 work_pos = viewport->WorkPos; // Use work area to avoid menu-bar/task-bar, if any!
+                ImVec2 work_size = viewport->WorkSize;
+                ImVec2 window_pos, window_pos_pivot;
+                window_pos.x = work_pos.x + work_size.x - PAD;
+                window_pos.y = work_pos.y + PAD;
+                window_pos_pivot.x = 1.0f;
+                window_pos_pivot.y = 0.0f;
+                ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
+                window_flags |= ImGuiWindowFlags_NoMove;
+                ImGui::SetNextWindowBgAlpha(0.35f); // Transparent background
+                if (ImGui::Begin("Example: Simple overlay", NULL, window_flags))
+                {
+                    ImGui::Text("%.3f ms (%.1f FPS)", dt.GetMiliseconds(), 1.0f / dt.GetSeconds());
+                }
+                ImGui::End();
             }
-            ImGui::End();
 
             for (auto layer : m_LayerStack)
                 layer->OnUpdate(dt);
