640c0a7efaf59923b9f3875bd3efdb3491b5974e
TekMike365 <miskosklenka@gmail.com>
2025-10-26 11:48:10 +0100

    modified the project structure

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -1,11 +1,4 @@
-#include <Log.hpp>
-#include <Test.hpp>
+#include <Voxy/Log.hpp>
+#include <Voxy/Test.hpp>
 
-int main(void) {
-    SayHello();
-
-    Voxy::Log::Trace("This is amazing!");
-
-    Voxy::Log::Logger l("TestSys");
-    l.Info("It werks! :3");
-}
+int main(void) { Voxy::Init(); }
---
+++ Voxy/src/Voxy/Test.cpp
@@ -0,0 +1,5 @@
+#include "Test.hpp"
+
+#include "Log.hpp"
+
+void Voxy::Init() { Voxy::Log::Init(); }
