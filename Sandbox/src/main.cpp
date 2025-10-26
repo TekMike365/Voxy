c7588ad6f75a3a0ca423af5e204f7bcbb6926c07
TekMike365 <miskosklenka@gmail.com>
2025-10-26 12:02:22 +0100

    the core structure

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -1,6 +1,12 @@
+#include <Voxy/Application.hpp>
 #include <Voxy/Voxy.hpp>
 
 int main(void) {
     Voxy::Init();
+
+    auto *app = new Voxy::Application();
+    app->Run();
+    delete app;
+
     Voxy::Terminate();
 }
